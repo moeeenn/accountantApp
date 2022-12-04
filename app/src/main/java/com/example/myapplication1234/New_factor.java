5f675272b323aacdd137d1890588cae74aa94549
moeeenn <moin1381.mm@gmail.com>
2022-12-04 23:12:46 +0330

    initial commit

--- app/src/main/java/com/example/myapplication1234/New_factor.java
+++ app/src/main/java/com/example/myapplication1234/New_factor.java
@@ -3,12 +3,32 @@ package com.example.myapplication1234;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class New_factor extends AppCompatActivity {
 
+    Spinner category,product;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_factor);
+        String[] arraySpinner = new String[]{
+                "PlaceHolder_1","PlaceHolder_2","PlaceHolder_3",
+        };
+        Spinner s = (Spinner) findViewById(R.id.spinner_cat);
+        ArrayAdapter adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,arraySpinner);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter1);
+
+        String[] arraySpinner2 = new String[]{
+                "PlaceHolder_1","PlaceHolder_2","PlaceHolder_3",
+        };
+        Spinner s1 = (Spinner) findViewById(R.id.spinner_pr);
+        ArrayAdapter adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,arraySpinner);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(adapter2);
     }
+
+
 }
